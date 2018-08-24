45618e32de72c36b95ff8c66a6e145c94d5aa90d
shun lee <leeshun1230@gmail.com>
2018-08-24 15:03:02 +0800

    fix gpu show error

--- ktqueue/api/node.py
+++ ktqueue/api/node.py
@@ -22,6 +22,28 @@ class NodesHandler(tornado.web.RequestHandler):
                 result += num
             return result
 
+        gpu_usages = await self.k8s_client.call_api(
+            api='/api/v1/namespaces/ktqueue/pods',
+            method='GET',
+        )
+
+        gpu_dict = {}
+        for gpu_usage in gpu_usages['items']:
+            spec = gpu_usage.get('spec', {})
+            if spec:
+                gpu = sum(
+                    [
+                        int(c.get('resources', {}).get('limits', {}).get('nvidia.com/gpu', '0'))
+                        for c in spec.get('containers', [])
+                    ]
+                )
+                node_name = spec.get('nodeName', '')
+                status = gpu_usage.get('status', {}).get('phase', '')
+                if status and node_name and gpu and status == 'Running':
+                    gpu_dict[node_name] = gpu_usage.get(node_name, 0) + gpu
+            else:
+                continue
+        
         ret = await self.k8s_client.call_api(
             api='/api/v1/nodes',
             method='GET',
@@ -30,7 +52,7 @@ class NodesHandler(tornado.web.RequestHandler):
         self.write({'items': [{
             'name': node['metadata']['name'],
             'labels': node['metadata']['labels'],
-            'gpu_used': used_gpus(node['metadata']['name']),
+            'gpu_used': gpu_dict.get(node['metadata']['name'], 0),
             'jobs': node_used_gpus[node['metadata']['name']],
             'gpu_capacity': node['status']['capacity'].get('nvidia.com/gpu', 0),
         } for node in ret['items']
